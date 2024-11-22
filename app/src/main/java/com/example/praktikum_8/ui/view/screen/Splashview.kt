d9af6a830d3514ccb52fa86226dfbe65323650fb
Wahyu <firmansyahw503@gmail.com>
2024-11-22 13:28:15 +0700

    Commit14 (Membuat Tombol)

--- app/src/main/java/com/example/praktikum_8/ui/view/screen/Splashview.kt
+++ app/src/main/java/com/example/praktikum_8/ui/view/screen/Splashview.kt
@@ -6,8 +6,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -39,5 +42,15 @@ fun Splashview(
             modifier = Modifier.size(150.dp)
         )
         Spacer(modifier = Modifier.padding(16.dp))
+        Button(
+            onClick = {
+                onMulaiButton()
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(32.dp),
+        ){
+            Text(text = "Mulai")
+        }
     }
 }
