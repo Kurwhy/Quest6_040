ffbed5d5d17865f27c7484b03de02a3a19e8c7b0
Wahyu <firmansyahw503@gmail.com>
2024-11-22 12:44:22 +0700

    Commit1 (membuat package data dan membuat objek MataKuliah)

---
+++ app/src/main/java/com/example/praktikum_8/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.praktikum_8.data
+
+object MataKuliah {
+    val option = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+}
